3a4f865724515838c2eccd1010315fba44c73296
lawrencejews <lawrencejews1@gmail.com>
2023-10-21 13:11:49 +0900

    added mock data to the database

--- server/index.js
+++ server/index.js
@@ -15,6 +15,11 @@ import { createPost } from "./controllers/posts.js";
 import { register } from "./controllers/auth.js";
 import { verifyToken } from "./middleware/auth.js";
 
+// MODELS 
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
+
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -58,4 +63,9 @@ mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
 }).then(() => {
   app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+  // MOCK DATA INJECTION JUST ONE TIME
+  // User.insertMany(users);
+  // Post.insertMany(posts);
+
 }).catch((error) => console.log(`${error} did not connect`));
