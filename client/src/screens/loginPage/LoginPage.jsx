fbf1dd0b12cc67e697aaa9e835ba47df01834ece
lawrencejews <lawrencejews1@gmail.com>
2023-10-26 22:38:34 +0900

    added login and register

--- client/src/screens/loginPage/LoginPage.jsx
+++ client/src/screens/loginPage/LoginPage.jsx
@@ -1,4 +1,5 @@
 import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 
 const LoginPage = () => {
 
@@ -13,17 +14,24 @@ const LoginPage = () => {
         p="1rem 6%"
         textAlign="center"
       >
-        <Typography  fontWeight="bold"  fontSize="32px"color="primary">
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
           SocialApp
         </Typography>
       </Box>
 
       <Box
-      
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
       >
-
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to SocialApp, the Social Media for Sociopaths!
+        </Typography>
+        <Form />
       </Box>
-   </Box>
+    </Box>
   )
 }
 
