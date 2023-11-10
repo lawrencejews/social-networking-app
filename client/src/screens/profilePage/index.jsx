25a2364019f054217debbdc7e7acd444a5fa2ee9
lawrencejews <lawrencejews1@gmail.com>
2023-11-10 23:15:16 +0900

    update profilePage

--- client/src/screens/profilePage/index.jsx
+++ client/src/screens/profilePage/index.jsx
@@ -1,16 +1,33 @@
 import { Box, useMediaQuery } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import Navbar from "screens/navbar";
-import UserWidget from "screens/widgets/UserWidget";
-import MyPostWidget from "screens/widgets/MyPostWidget";
-import PostsWidget from "screens/widgets/PostsWidget";
-import AdvertWidget from "screens/widgets/AdvertWidget";
-import FriendListWidget from "screens/widgets/FriendListWidget";
+import { useParams } from "react-router-dom";
+import Navbar from "scenes/navbar";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import UserWidget from "scenes/widgets/UserWidget";
 
 const ProfilePage = () => {
-
+  const [user, setUser] = useState(null);
+  const { userId } = useParams();
+  const token = useSelector((state) => state.token);
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
-  const { _id, picturePath } = useSelector((state) => state.user);
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (!user) return null;
 
   return (
     <Box>
@@ -19,30 +36,25 @@ const ProfilePage = () => {
         width="100%"
         padding="2rem 6%"
         display={isNonMobileScreens ? "flex" : "block"}
-        gap="0.5rem"
-        justifyContent="space-between"
+        gap="2rem"
+        justifyContent="center"
       >
         <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
-          <UserWidget userId={_id} picturePath={picturePath} />
+          <UserWidget userId={userId} picturePath={user.picturePath} />
+          <Box m="2rem 0" />
+          <FriendListWidget userId={userId} />
         </Box>
         <Box
           flexBasis={isNonMobileScreens ? "42%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
-          <MyPostWidget picturePath={picturePath} />
-          <PostsWidget userId={_id} />
+          <MyPostWidget picturePath={user.picturePath} />
+          <Box m="2rem 0" />
+          <PostsWidget userId={userId} isProfile />
         </Box>
-        {isNonMobileScreens && (
-          <Box flexBasis="26%">
-            <AdvertWidget />
-            <Box m="2rem 0" />
-            <FriendListWidget userId={_id} />
-          </Box>
-        )}
       </Box>
     </Box>
   );
 };
-
 
 export default ProfilePage;
